5c7693bdf8bf6762891574f879cb5484617ae660
Miles Dobbins <mdobbins344@gmail.com>
2020-03-08 22:27:47 -0400

    trying to add app.vue and main.js to main page

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view />
+  </div>
+</template>
+
+<script>
+  import Header from "./components/Homepage/Header";
+ // import Homepage from "./components/Homepage/Homepage";
+  //import RestaurantsPage from "./components/RestaurantsPage/RestaurantsPage";
+  export default {
+  name: 'App',
+  components: {
+    //RestaurantsPage,
+    //Homepage,
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
